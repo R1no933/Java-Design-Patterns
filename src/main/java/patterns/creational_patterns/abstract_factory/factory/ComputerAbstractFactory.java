56272a8ad1e419bface99f3e7bd1c3f500a937fc
R1no933 <dbaskakov93@yandex.ru>
2023-08-10 20:04:13 +0300

    PATTERN03: complete example for abstract factory pattern

---
+++ src/main/java/patterns/creational_patterns/abstract_factory/factory/ComputerAbstractFactory.java
@@ -0,0 +1,7 @@
+package patterns.creational_patterns.abstract_factory.factory;
+
+import patterns.creational_patterns.abstract_factory.models.Computer;
+
+public interface ComputerAbstractFactory {
+    public Computer createomputer();
+}
