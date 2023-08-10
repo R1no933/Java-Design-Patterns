3053c2a7a66241be7d35c53c0d58a0228cfc5282
R1no933 <dbaskakov93@yandex.ru>
2023-08-10 22:33:48 +0300

    fix reviwer remarks

--- src/main/java/patterns/creational_patterns/factory/ComputerFactory.java
+++ src/main/java/patterns/creational_patterns/factory/ComputerFactory.java
@@ -6,9 +6,11 @@ import patterns.creational_patterns.factory.models.Server;
 
 public class ComputerFactory {
     public static Computer getComputer(String type, String ram, String hdd, String cpu) {
-        if ("PC".equalsIgnoreCase(type)) return new PC(ram, hdd, cpu);
-        else if ("Server".equalsIgnoreCase(type)) return new Server(ram, hdd, cpu);
-
+        if ("PC".equalsIgnoreCase(type)) {
+            return new PC(ram, hdd, cpu);
+        } else if ("Server".equalsIgnoreCase(type)) {
+            return new Server(ram, hdd, cpu);
+        }
         return null;
     }
 }
