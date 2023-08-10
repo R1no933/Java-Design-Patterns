1402a9127960dcbed554f5c9823f7356c1e5ec8b
R1no933 <dbaskakov93@yandex.ru>
2023-08-10 19:34:39 +0300

    PATTERN02: complete example for factory pattern

---
+++ src/main/java/patterns/creational_patterns/factory/ComputerFactory.java
@@ -0,0 +1,14 @@
+package patterns.creational_patterns.factory;
+
+import patterns.creational_patterns.factory.models.Computer;
+import patterns.creational_patterns.factory.models.PC;
+import patterns.creational_patterns.factory.models.Server;
+
+public class ComputerFactory {
+    public static Computer getComputer(String type, String ram, String hdd, String cpu) {
+        if ("PC".equalsIgnoreCase(type)) return new PC(ram, hdd, cpu);
+        else if ("Server".equalsIgnoreCase(type)) return new Server(ram, hdd, cpu);
+
+        return null;
+    }
+}
